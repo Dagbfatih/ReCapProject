4bf53cb5dc37d75f74daade4e013bbba1ac4b19f
Dagbfatih <Dagbfatih@users.noreply.github.com>
2021-04-04 22:50:41 +0300

    2

--- Entities/Concrete/CarImage.cs
+++ Entities/Concrete/CarImage.cs
@@ -1,13 +1,14 @@
 ﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Entities.Concrete
 {
     public class CarImage : IEntity
     {
-        public int ImageId { get; set; }
+        public int Id { get; set; }
         public int CarId { get; set; }
         public string ImagePath { get; set; }
         public DateTime Date { get; set; }
